f50badb617bd81e1c00d4344d7729544aa1824e6
valva-ro <vavarela.rodriguez@gmail.com>
2021-05-20 17:05:15 -0300

    [ADD] download wireframes option in navbar

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+console.log(navLink);
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(link => {
+    link.addEventListener("click", closeMenu)
+    console.log(link);
+});
+
+function mobileMenu() {
+    navMenu.classList.toggle("active");
+    hamburger.classList.toggle("active");
+}
+
+function closeMenu() {
+    navMenu.classList.remove("active");
+    hamburger.classList.remove("active");
+}
