b0de883dd72403a4d7822c3ee00fe81a988e33ee
valva-ro <vavarela.rodriguez@gmail.com>
2021-06-10 23:17:18 -0300

    [FIX] path to some files

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("menu");
-const navLink = document.querySelectorAll(".nav-link");
-
-console.log(navLink);
-
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(link => {
-    link.addEventListener("click", closeMenu)
-    console.log(link);
-});
-
-function mobileMenu() {
-    navMenu.classList.toggle("active");
-    hamburger.classList.toggle("active");
-}
-
-function closeMenu() {
-    navMenu.classList.remove("active");
-    hamburger.classList.remove("active");
+window.onload = function() {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("menu");
+    const navLink = document.querySelectorAll(".nav-link");
+    
+    console.log(navLink);
+    
+    hamburger.addEventListener("click", mobileMenu);
+    navLink.forEach(link => {
+        link.addEventListener("click", closeMenu)
+        console.log(link);
+    });
+    
+    function mobileMenu() {
+        navMenu.classList.toggle("active");
+        hamburger.classList.toggle("active");
+    }
+    
+    function closeMenu() {
+        navMenu.classList.remove("active");
+        hamburger.classList.remove("active");
+    }
 }
